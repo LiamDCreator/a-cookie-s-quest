8b7957b6b5ac17bef542898c206f58954a554683
LiamDCreator <Liamduijn@gmail.com>
2024-10-09 22:10:35 +0200

    added player plus controls

---
+++ Assets/playerScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public float movespeed;
+    public Vector3 upOrDown = Vector3.up;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           transform.Translate(upOrDown * movespeed * Time.deltaTime);
+
+           if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
+            upOrDown = Vector3.down;
+           }
+
+            if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+            upOrDown = Vector3.up;
+           }
+    }
+}
