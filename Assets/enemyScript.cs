8af0eeb221adae0a12cb03f4e12eeaa61be36106
LiamDCreator <Liamduijn@gmail.com>
2024-10-17 11:08:32 +0200

    added moving enemy

---
+++ Assets/enemyScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+    public float moveSpeed;
+    public float speed;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   void Update()
+{
+    // Move left at a constant speed
+    transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+    // Move towards the player, but not affecting the leftward movement
+    Vector3 targetPosition = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    transform.position = new Vector3(transform.position.x, targetPosition.y, targetPosition.z);
+}}
