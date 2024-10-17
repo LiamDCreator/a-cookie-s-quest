8e7f3a0df1a53314d5f7f1a366a60b962d2e5ed8
LiamDCreator <Liamduijn@gmail.com>
2024-10-17 11:17:01 +0200

    created prefabs out of enemies

--- Assets/fatPersonScript.cs
+++ Assets/fatPersonScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class fatPersonScript : MonoBehaviour
 {
-    public int movespeed;
+    public float movespeed;
     // Start is called before the first frame update
     void Start()
     {
