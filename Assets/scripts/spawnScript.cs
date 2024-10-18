721f3c35dda5f2b8dc1addfff531d5e8bee284fb
LiamDCreator <Liamduijn@gmail.com>
2024-10-18 17:12:25 +0200

    begonnen spawn system

--- Assets/scripts/spawnScript.cs
+++ Assets/scripts/spawnScript.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class spawnScript : MonoBehaviour
 {
+    public GameObject wall;
+    public GameObject fatPerson;
+    public GameObject walkingPerson;
+    public float spawnRate = 2;
+    private float timer = 0;
+    public float heightOffset = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,13 @@ public class spawnScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer < spawnRate){
+
+            timer = timer + Time.deltaTime;
+        } else {
+            Instantiate(fatPerson, transform.position, transform.rotation);
+            timer = 0;
+        }
     }
+    void spawnObject(){}
 }
