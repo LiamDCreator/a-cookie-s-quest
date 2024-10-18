d7e16e1062cbc2f54faf3474b360987706b325c7
LiamDCreator <Liamduijn@gmail.com>
2024-10-18 17:20:46 +0200

    spawn system wall

--- Assets/scripts/spawnScript.cs
+++ Assets/scripts/spawnScript.cs
@@ -7,13 +7,13 @@ public class spawnScript : MonoBehaviour
     public GameObject wall;
     public GameObject fatPerson;
     public GameObject walkingPerson;
-    public float spawnRate = 2;
+    public float spawnRate = 3;
     private float timer = 0;
-    public float heightOffset = 10;
+    public float heightOffset = 4;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnObject();
     }
 
     // Update is called once per frame
@@ -23,9 +23,14 @@ public class spawnScript : MonoBehaviour
 
             timer = timer + Time.deltaTime;
         } else {
-            Instantiate(fatPerson, transform.position, transform.rotation);
+           spawnObject();
             timer = 0;
         }
     }
-    void spawnObject(){}
+    void spawnObject(){
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+
+    Instantiate(wall, new Vector3(transform.position.x,Random.Range(lowestPoint,highestPoint), 0), transform.rotation);
+    }
 }
