d671dc8c1fded77382e343f79fa7f0443e54eea3
LiamDCreator <Liamduijn@gmail.com>
2024-10-22 16:11:34 +0200

    made code clearer

--- Assets/scripts/playerScore.cs
+++ Assets/scripts/playerScore.cs
@@ -8,15 +8,10 @@ public class playerScore : MonoBehaviour
 {  
     public Text playerScoreNow;
     public int currentScore = 0;
-    // Start is called before the first frame update
-   void Start(){
-    playerScoreNow.text = currentScore.ToString();
-  }
-
-    // Update is called once per frame
-   
+ 
     private void OnTriggerEnter2D(Collider2D collision){
         currentScore++;
-        playerScoreNow.text = currentScore.ToString();
+       
+       
     }
 }
