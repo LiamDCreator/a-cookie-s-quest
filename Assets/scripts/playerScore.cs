bf8d39af845f669062bd7f7222450b5342553aa1
LiamDCreator <Liamduijn@gmail.com>
2024-10-27 13:28:37 +0100

    player point sound

--- Assets/scripts/playerScore.cs
+++ Assets/scripts/playerScore.cs
@@ -7,11 +7,13 @@ using UnityEngine.UI;
 public class playerScore : MonoBehaviour
 {  
     public Text playerScoreNow;
+    public AudioSource playerPointSound;
+
     public int currentScore = 0;
  
     private void OnTriggerEnter2D(Collider2D collision){
         currentScore++;
          playerScoreNow.text = currentScore.ToString();
-       
+       playerPointSound.Play();
     }
 }
