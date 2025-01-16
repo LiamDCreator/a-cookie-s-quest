0bf47b500cb6233aaa1a0e23e72ab5956500fec8
LiamDCreator <Liamduijn@gmail.com>
2025-01-16 17:24:18 +0100

    cookie animatie

--- Assets/cookieAnimatie.cs
+++ Assets/cookieAnimatie.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class cookieAnimatie : MonoBehaviour
 {
+    public float movespeed;
+    public GameObject tapSign;
+    public GameObject bottonTapSign;
+    public Vector3 upOrDown = Vector3.up;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,24 @@ public class cookieAnimatie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+           transform.Translate(upOrDown * movespeed * Time.deltaTime);
+        if(transform.position.y >= 2.2){
+            upOrDown = Vector3.down;
+        }
+         if(transform.position.y <= -2.5){
+            upOrDown = Vector3.up;
+        }
+         if(transform.position.y >= 1.9){
+        tapSign.SetActive(true);
+        } else {
+        tapSign.SetActive(false);
+
+        }
+         if(transform.position.y <= -2.2){
+        bottonTapSign.SetActive(true);
+        } else {
+        bottonTapSign.SetActive(false);
+
+        }
     }
 }
