7998fd310a3f5f48a00861a49eacfaf29d73db4e
LiamDCreator <Liamduijn@gmail.com>
2025-03-12 15:33:55 +0100

    optimized enemyscript and moved all scripts to script folder

--- Assets/scripts/enemyScripts/enemyScript.cs
+++ Assets/scripts/enemyScripts/enemyScript.cs
@@ -4,35 +4,30 @@ using UnityEngine;
 
 public class enemyScript : MonoBehaviour
 {
-    private int rand;
-     public float moveSpeed;
-    public float speed;
-    public Transform player;
-    public Sprite[] SpritePic;
+   
+     [SerializeField] private float moveSpeed;
+    [SerializeField] private float speed;
+    [SerializeField] private Sprite[] SpritePic;
+    private Transform player;
+    
 
-    // Start is called before the first frame update
+   
     void Start()
     {
-        rand = Random.Range(0,SpritePic.Length);
-        GetComponent<SpriteRenderer>().sprite = SpritePic[rand];
-        // Automatically find the player object with the "Player" tag in the scene
-        if (player == null)
-        {
+        GetComponent<SpriteRenderer>().sprite = SpritePic[Random.Range(0,SpritePic.Length)];
+       
             GameObject playerObject = GameObject.FindWithTag("Player");
             if (playerObject != null)
             {
                 player = playerObject.transform;
             }
-        }
+        
     }
-
-    // Update is called once per frame
     void Update()
     {
-        // Move left at a constant speed
+   
         transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
 
-        // Move towards the player, but not affecting the leftward movement
         if (player != null)
         {
             Vector3 targetPosition = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
