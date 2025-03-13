5486e7401f548213547115e28341b121a2464703
LiamDCreator <Liamduijn@gmail.com>
2025-03-13 16:16:18 +0100

    removed onnodige script en optimized particlescript

--- Assets/scripts/particleScript.cs
+++ Assets/scripts/particleScript.cs
@@ -1,15 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class particleScript : MonoBehaviour
 {
-    public Transform playerPosition;
-    public ParticleSystem playerParticles;
-    // Start is called before the first frame update
-   
-
-    // Update is called once per frame
+    [SerializeField] private Transform playerPosition;
+    [SerializeField] private ParticleSystem playerParticles;
    
    public void playerDiesParticles(){
     transform.position = playerPosition.position;
